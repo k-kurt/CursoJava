a28d2f3f41dd37ab5c074e0c2e6f7b84f043b2ec
kevin sboboda <sboboda1997@gmail.com>
2022-04-05 14:38:50 -0300

    Video 89-formulario usuario, contraseña. Cambio de color del JTextField segun cantidad de caracteres

--- Video88EventosEnCuadrosDeTextos/src/EventosEnCuadrosDeTextoII.java
+++ Video88EventosEnCuadrosDeTextos/src/EventosEnCuadrosDeTextoII.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -11,6 +12,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 
 public class EventosEnCuadrosDeTextoII {
 
@@ -43,33 +47,69 @@ public class EventosEnCuadrosDeTextoII {
 		 JPanel lamina2=new JPanel();
 		 lamina2.setLayout(new GridLayout(2,2));
 		 
+		 panelInterno Guardar=new panelInterno();
+		 
+		 
 		 usuario=new JLabel("Usuario: ");
 		 CuadroUsuario=new JTextField(50);
 		 contraseña=new JLabel("Contraseña: ");
 		 CadroContraseña=new JPasswordField(50);
+		 Document contra=CadroContraseña.getDocument();
+		 contra.addDocumentListener(Guardar);
 		 
 		 lamina2.add(usuario);
 		 lamina2.add(CuadroUsuario);
 		 lamina2.add(contraseña);
 		 lamina2.add(CadroContraseña);
 		 
+		 
+		 
 		 botonEnviar=new JButton("ENVIAR");
 		 add(botonEnviar, BorderLayout.SOUTH);
+		 
+		 
 		 
 		 add(lamina2, BorderLayout.NORTH);
 	 }
 	 
 	 
 	 
-	 private class panelInterno implements ActionListener{
+	 private class panelInterno implements DocumentListener{
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void insertUpdate(DocumentEvent e) {
+			// TODO Auto-generated method stub
+			char [] contrasenia;
+			contrasenia= CadroContraseña.getPassword();
+			
+			if(contrasenia.length<8 || contrasenia.length> 12) {
+				CadroContraseña.setBackground(Color.RED);
+			}else {
+				CadroContraseña.setBackground(Color.WHITE);
+			}
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			// TODO Auto-generated method stub
+			char [] contrasenia;
+			contrasenia= CadroContraseña.getPassword();
+			
+			if(contrasenia.length<8 || contrasenia.length> 12) {
+				CadroContraseña.setBackground(Color.RED);
+			}else {
+				CadroContraseña.setBackground(Color.WHITE);
+			}
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
 			// TODO Auto-generated method stub
 			
 		}
 		 
 	 }
+		 
 	 private JLabel usuario;
 	 private JLabel contraseña;
 	 private JTextField CuadroUsuario;
