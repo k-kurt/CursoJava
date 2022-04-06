a3355193bb36cbcceff0b48d17949afe642f5816
Kevin <sboboda1997@gmail.com>
2022-04-06 12:12:47 -0300

    Video 90

---
+++ Video90AreasDeTextosI/src/video90.java
@@ -0,0 +1,68 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class video90 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		marcoAreaText marco=new marcoAreaText();
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marco.setVisible(true);
+		
+	}
+
+}
+
+
+class marcoAreaText extends JFrame{
+	
+	public marcoAreaText() {
+		setTitle("video 90");
+		setBounds(100, 100, 800, 600);
+		panelI lamina=new panelI();
+		add(lamina);
+	}
+	
+}
+
+
+
+class panelI extends JPanel{
+	public panelI() {
+		 cuadro=new JTextArea(8,15);
+		JScrollPane laminaSc=new JScrollPane(cuadro);//sale barra de scroll a lo ancho y largo en el elemento que pasamos como parametro
+		
+		cuadro.setLineWrap(true);
+		boton=new JButton("Guardar");
+		add(laminaSc);
+		add(boton);
+		
+		boton.addActionListener(new laminainterna());
+		
+	}
+	
+	private class laminainterna implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println(cuadro.getText());
+		}
+		
+	}
+	
+	
+
+	
+	private JTextArea cuadro;
+	private JButton boton;
+}
+
