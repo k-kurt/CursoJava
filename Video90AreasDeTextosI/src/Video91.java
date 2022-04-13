f7d2142eed7417ff4fd3c3bdc88bc28cee964527
kevin sboboda <sboboda1997@gmail.com>
2022-04-12 21:57:41 -0300

    Video 91

--- Video90AreasDeTextosI/src/Video91.java
+++ Video90AreasDeTextosI/src/Video91.java
@@ -2,6 +2,8 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -31,25 +33,54 @@ public class Video91 {
 				setBounds(100,100,800,500);
 				setLayout(new BorderLayout());
 				
-				JPanel lamina1=new JPanel();
-				lamina1.setLayout(new FlowLayout());
-				
-				 cuadro1=new JTextArea(50,50);
-				
-				lamina1.add(cuadro1);
-				
-				add(lamina1, BorderLayout.CENTER);
+		
+				 cuadro1=new JTextArea(10,30);
+				 laminaConBarras =new JScrollPane(cuadro1);
+	
+				 
+				add(laminaConBarras, BorderLayout.CENTER);
 				
 				JPanel lamina2=new JPanel();
 				lamina2.setLayout(new GridLayout(1,2));
 				
 				 agregar=new JButton("AGREGAR");
-				 activar=new JButton("ACTIVAR/DESACTIVAR");
+				agregar.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						cuadro1.append("Este es el texto que vamos a agregar en el documento");
+					}
+					
+				}); 
 				
+				
+				 activar=new JButton("ACTIVAR/DESACTIVAR");
+				activar.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+				
+						boolean saltar=!cuadro1.getLineWrap();
+						cuadro1.setLineWrap(saltar);
+						
+						if(saltar) {
+							activar.setText("Quitar salto");
+						}else {
+							activar.setText("Activar salto");
+						}
+						
+					}
+				});
+				 
 				lamina2.add(agregar);
+			
 				lamina2.add(activar);
 				
 				add(lamina2,BorderLayout.SOUTH);
+				
+				
 				
 				
 				
@@ -57,6 +88,10 @@ public class Video91 {
 			
 			private JButton agregar, activar;
 			private JTextArea cuadro1;
+			private JScrollPane laminaConBarras;
+			
+			
+	
 			
 		}
 
