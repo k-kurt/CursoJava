7ce9ca1acf76e83c0548b33a1997204854006b34
kevin sboboda <sboboda1997@gmail.com>
2022-04-14 20:31:14 -0300

    Video 92 CheckBox

--- Video92ComponentesSwingCheckBox/src/video92.java
+++ Video92ComponentesSwingCheckBox/src/video92.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -35,9 +38,16 @@ class panelPrincipal extends JPanel{
 		
 		setLayout(new BorderLayout());
 		
+		
+		Font miletra=new Font("Serif",Font.PLAIN,24);
+		
+		checkListener loschecks =new checkListener();
+		
+		
 		texto=new JLabel("texto al que voy a modificar",JLabel.CENTER);//JLabel.CENTER permite poner el JLabel en el centro
 		//si no lo ponemos queda a la izquierda 
 
+		texto.setFont(miletra);
 		add(texto,BorderLayout.NORTH);
 	
 		
@@ -45,13 +55,36 @@ class panelPrincipal extends JPanel{
 		//lamina2.setLayout(new GridLayout(1,2));
 		
 		check1 =new JCheckBox("Activar Negrita");
+		check1.addActionListener(loschecks);
+		//check1.addActionListener(new checkListener());
 		check2=new JCheckBox("Activar Cursiva");
+		check2.addActionListener(loschecks);
+		//check2.addActionListener(new checkListener());
 		
 		lamina2.add(check1);
 		lamina2.add(check2);
 		
 		add(lamina2,BorderLayout.SOUTH);
 	}
+	
+	
+	private class checkListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+			int tipo=0;
+			
+			if(check1.isSelected()) tipo+=Font.BOLD;
+			if(check2.isSelected()) tipo+=Font.ITALIC;
+			
+			texto.setFont(new Font("Serif",tipo,24));
+			
+		}
+		
+	}
+	
 	
 	private JLabel texto;
 	private JCheckBox check1, check2;
