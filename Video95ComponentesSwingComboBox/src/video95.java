f60672367473dd524a8c09c3edda9bf052ecb2ee
kevin sboboda <sboboda1997@gmail.com>
2022-04-24 19:34:34 -0300

    solucion de error con lamina

--- Video95ComponentesSwingComboBox/src/video95.java
+++ Video95ComponentesSwingComboBox/src/video95.java
@@ -1,4 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class video95 {
@@ -6,8 +11,9 @@ public class video95 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		marcovideo95 marco=new marcovideo95();
-		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		marco.setVisible(true);
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 }
@@ -16,13 +22,22 @@ class marcovideo95 extends JFrame{
 	public marcovideo95() {
 		setTitle("Video 95");
 		setBounds(200, 100, 800, 500);
-		JPanel lamina=new JPanel();
+		panelvideo95 lamina=new panelvideo95();
 		add(lamina);
 	}
 }
 
 class panelvideo95 extends JPanel{
 	public panelvideo95() {
+		setLayout(new BorderLayout());
+		texto=new JLabel("texto a modificar...",JLabel.CENTER);
+		texto.setFont(new Font("Serif",Font.PLAIN,18));
+		add(texto,BorderLayout.CENTER);
+		JButton boton=new JButton("hola");
+		add(boton,BorderLayout.SOUTH);
 		
 	}
+	private JLabel texto;
+	
+	
 }
