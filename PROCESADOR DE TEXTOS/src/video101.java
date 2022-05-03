3997d4569f46a677b510576c9b8dc1d905fb0095
kevin sboboda <sboboda1997@gmail.com>
2022-05-03 20:53:59 -0300

    Parte del video 102

--- PROCESADOR DE TEXTOS/src/video101.java
+++ PROCESADOR DE TEXTOS/src/video101.java
@@ -1,11 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 
 public class video101 {
 
@@ -31,10 +36,8 @@ class marcoGeneral extends JFrame{
 class panelPrincipal extends JPanel{
 	public panelPrincipal() {
 		setLayout(new BorderLayout());
-		
-		
-		
 		JPanel lamina2=new JPanel();
+		eventos loseventos=new eventos();
 		//-------------------------------------------------------------
 		menu=new JMenuBar();
 		fuente=new JMenu("Fuentes");
@@ -43,6 +46,7 @@ class panelPrincipal extends JPanel{
 		//-------------------------------------------------------------
 		fuente.add(arial=new JMenuItem("Arial"));
 		fuente.add(courier=new JMenuItem("Courier"));
+		courier.addActionListener(loseventos);
 		fuente.add(verdana=new JMenuItem("Verdana"));
 		//-------------------------------------------------------------
 		estilo.add(negrita=new JMenuItem("Negrita"));
@@ -53,15 +57,26 @@ class panelPrincipal extends JPanel{
 		tamanio.add(t20=new JMenuItem("20"));
 		tamanio.add(t24=new JMenuItem("24"));
 		//-------------------------------------------------------------
-		
 		menu.add(fuente);
 		menu.add(estilo);
 		menu.add(tamanio);
 		lamina2.add(menu);
 		add(lamina2,BorderLayout.NORTH);
+		laminaTexto=new JTextPane();
+		add(laminaTexto,BorderLayout.CENTER);
+	}
+	
+	private class eventos implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			laminaTexto.setFont(new Font("Courier",Font.PLAIN,12));
+		}
 		
 	}
 	
+	private JTextPane laminaTexto;
 	private JMenuBar menu;
 	private JMenuItem arial,courier,verdana,negrita,cursiva,t12,t16,t20,t24;
 	private JMenu fuente,estilo,tamanio;
