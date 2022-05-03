29dc5a12105b54b90fa80817317dde6092a657e6
kevin sboboda <sboboda1997@gmail.com>
2022-05-02 21:05:49 -0300

    Video 100

--- Video100ComponentesSwingCreacionDeMenus/src/Video100.java
+++ Video100ComponentesSwingCreacionDeMenus/src/Video100.java
@@ -1,4 +1,7 @@
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class Video100 {
@@ -25,6 +28,41 @@ class marcomenu extends JFrame{
 
 class panelmenu extends JPanel{
 	public panelmenu() {
+		JMenuBar barra=new JMenuBar();
+		
+		JMenu archivo=new JMenu("Archivo");
+		JMenu edicion=new JMenu("Edicion");
+		JMenu herramientas=new JMenu("Herramientas");
+		JMenu opciones=new JMenu("Opciones");
+		
+		
+		JMenuItem guardar=new JMenuItem("Guardar");
+		JMenuItem guardar_como=new JMenuItem("Guardar Como...");
+		JMenuItem cortar=new JMenuItem("Cortar");
+		JMenuItem editar=new JMenuItem("Editar");
+		JMenuItem pegar=new JMenuItem("Pegar");
+		JMenuItem general=new JMenuItem("Generales");
+		JMenuItem opcion1=new JMenuItem("Opcion 1");
+		JMenuItem opcion2=new JMenuItem("Opccion 2");
+		
+		opciones.add(opcion1);
+		opciones.add(opcion2);
+		
+		archivo.add(guardar);
+		archivo.add(guardar_como);
+		edicion.add(cortar);
+		edicion.add(editar);
+		edicion.add(pegar);
+		edicion.addSeparator();//separador
+		edicion.add(opciones);
+		herramientas.add(general);
+		
+		
+		barra.add(archivo);
+		barra.add(edicion);
+		barra.add(herramientas);
+		
+		add(barra);
 		
 	}
 }
