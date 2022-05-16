030e6347df52a01de31017fffe658ba2c39d30ca
Kevin <sboboda1997@gmail.com>
2022-05-16 11:02:14 -0300

    Video 108

--- Video108MenuConCheckBoxs&RadioButton/src/video108.java
+++ Video108MenuConCheckBoxs&RadioButton/src/video108.java
@@ -1,11 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.Font;
 
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JTextPane;
 import javax.swing.text.StyledEditorKit;
 
@@ -48,15 +52,54 @@ class panelMenu extends JPanel{
 		creacion_items("fuente", "Courier", "Courier", 9, 12);
 		creacion_items("fuente", "Verdana", "Verdana", 9, 12);
 		
+		//-----------------------------------------------------------------------------------------------------------
+
+//		creacion_items("estilo", "Negrita", "Bold", Font.BOLD, 12);
+//		creacion_items("estilo", "Cursiva", "", Font.ITALIC, 12);
+		JCheckBoxMenuItem negrita=new JCheckBoxMenuItem("Negrita",new ImageIcon("D:/Cursos Udemy/Java/CursoJava/PROCESADOR DE TEXTOS/src/iconos/texto-en-negrita.gif"));
+		JCheckBoxMenuItem cursiva=new JCheckBoxMenuItem("Cursiva",new ImageIcon("D:/Cursos Udemy/Java/CursoJava/PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif"));
+
+		negrita.addActionListener(new StyledEditorKit.BoldAction());
+		cursiva.addActionListener(new StyledEditorKit.ItalicAction());
 		
-		creacion_items("estilo", "Negrita", "Bold", Font.BOLD, 12);
-		creacion_items("estilo", "Cursiva", "", Font.ITALIC, 12);
+		estilo.add(negrita);
+		estilo.add(cursiva);
+		
+		//-----------------------------------------------------------------------------------------------------------
+
+		/*
+		 * creacion_items("tamanio", "16", "Italic", 9, 12); creacion_items("tamanio",
+		 * "20", "", 9, 20); creacion_items("tamanio", "24", "", 9, 24);
+		 * creacion_items("tamanio", "26", "", 9, 26);
+		 */
+		
+		ButtonGroup grupo=new ButtonGroup();
+		
+		JRadioButtonMenuItem t16=new JRadioButtonMenuItem("16");
+		JRadioButtonMenuItem t20=new JRadioButtonMenuItem("20");
+		JRadioButtonMenuItem t24=new JRadioButtonMenuItem("24");
+		JRadioButtonMenuItem t26=new JRadioButtonMenuItem("26");
+		
+		grupo.add(t16);
+		grupo.add(t20);
+		grupo.add(t24);
+		grupo.add(t26);
+		
+		t16.addActionListener(new StyledEditorKit.FontSizeAction("event", 16));
+		t20.addActionListener(new StyledEditorKit.FontSizeAction("event", 20));
+		t24.addActionListener(new StyledEditorKit.FontSizeAction("event", 24));
+		t26.addActionListener(new StyledEditorKit.FontSizeAction("event", 26));
+		
+		tamanio.add(t16);
+		tamanio.add(t20);
+		tamanio.add(t24);
+		tamanio.add(t26);
 		
 		
-		creacion_items("tamanio", "16", "Italic", 9, 12);
-		creacion_items("tamanio", "20", "", 9, 20);
-		creacion_items("tamanio", "24", "", 9, 24);
-		creacion_items("tamanio", "26", "", 9, 26);
+		//-----------------------------------------------------------------------------------------------------------
+
+		
+		
 		
 		laminaMenu.add(menu);
 		add(laminaMenu,BorderLayout.NORTH);
@@ -74,14 +117,12 @@ class panelMenu extends JPanel{
 			}else if(tipo_fuente=="Verdana") {
 				itemMenu.addActionListener(new StyledEditorKit.FontFamilyAction("event","Verdana"));
 			}
-		}else if(menu_pertenece=="estilo") {
-			estilo.add(itemMenu);
-			if(tipo_estilo==Font.BOLD) {
-				itemMenu.addActionListener(new StyledEditorKit.BoldAction());
-			}else if(tipo_estilo==Font.ITALIC) {
-				itemMenu.addActionListener(new StyledEditorKit.ItalicAction());
-			}
-		}else if(menu_pertenece=="tamanio") {
+		} /*
+			 * else if(menu_pertenece=="estilo") { estilo.add(itemMenu);
+			 * if(tipo_estilo==Font.BOLD) { itemMenu.addActionListener(new
+			 * StyledEditorKit.BoldAction()); }else if(tipo_estilo==Font.ITALIC) {
+			 * itemMenu.addActionListener(new StyledEditorKit.ItalicAction()); } }
+			 */else if(menu_pertenece=="tamanio") {
 			tamanio.add(itemMenu);
 			itemMenu.addActionListener(new StyledEditorKit.FontSizeAction("event", tamanio_letra));
 		}
