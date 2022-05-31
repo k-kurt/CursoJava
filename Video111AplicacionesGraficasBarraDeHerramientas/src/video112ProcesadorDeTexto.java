0a4afc2d697dfc3a7fb8324d9245c99a83e21c99
kevin sboboda <sboboda1997@gmail.com>
2022-05-31 20:00:53 -0300

    Video 114

--- Video111AplicacionesGraficasBarraDeHerramientas/src/video112ProcesadorDeTexto.java
+++ Video111AplicacionesGraficasBarraDeHerramientas/src/video112ProcesadorDeTexto.java
@@ -21,6 +21,7 @@ import javax.swing.text.StyledEditorKit;
 import javax.swing.text.StyledEditorKit.AlignmentAction;
 import javax.swing.text.StyledEditorKit.ForegroundAction;
 import javax.swing.text.StyledEditorKit.ItalicAction;
+import javax.swing.text.StyledEditorKit.UnderlineAction;
 
 public class video112ProcesadorDeTexto {
 
@@ -37,7 +38,7 @@ class marcoMenu extends JFrame {
 
 	public marcoMenu() {
 		setTitle("Video 108, añadiendo checkboxs y radioButtons al editor de texto");
-		setBounds(0,0,300, 800);
+		setBounds(0, 0, 300, 800);
 		panelMenu lamina = new panelMenu();
 		add(lamina);
 
@@ -66,7 +67,6 @@ class panelMenu extends JPanel {
 
 		creacion_items("estilo", "Negrita", "Bold", Font.BOLD, 12);
 		creacion_items("estilo", "Cursiva", "", Font.ITALIC, 12);
-		
 
 		ButtonGroup grupo = new ButtonGroup();
 
@@ -76,7 +76,7 @@ class panelMenu extends JPanel {
 		JRadioButtonMenuItem t26 = new JRadioButtonMenuItem("26");
 
 		t26.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_DOWN_MASK));
-		
+
 		grupo.add(t16);
 		grupo.add(t20);
 		grupo.add(t24);
@@ -107,51 +107,79 @@ class panelMenu extends JPanel {
 		cursivaE.addActionListener(new StyledEditorKit.ItalicAction());
 		emergente.add(negritaE);
 		emergente.add(cursivaE);
+		
 
 		laminaTexto.setComponentPopupMenu(emergente);
-		
-		
+
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		/*
+		 * JToolBar menubar=new JToolBar(); JButton cursivabar=new JButton(new
+		 * ImageIcon("C:/Users/genesis/Desktop/Cursos/JAVA/PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif"
+		 * .../PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif")); JButton
+		 * subrabar=new JButton(new ImageIcon("src/subrayada.png")); JButton
+		 * negritabar=new JButton(new
+		 * ImageIcon("C:/Users/genesis/Desktop/Cursos/JAVA/PROCESADOR DE TEXTOS/src/iconos/texto-en-negrita.gif"
+		 * .../PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif")); JButton
+		 * verde=new JButton(new ImageIcon("src/verde.png")); JButton rojo=new
+		 * JButton(new ImageIcon("src/rojo.png")); JButton azul=new JButton(new
+		 * ImageIcon("src/azul.png")); JButton centrado=new JButton(new
+		 * ImageIcon("src/centrado.png")); JButton izquierda=new JButton(new
+		 * ImageIcon("src/izquierda.png")); JButton derecha=new JButton(new
+		 * ImageIcon("src/derecha.png")); JButton justificado=new JButton(new
+		 * ImageIcon("src/justificar.png"));
+		 * 
+		 * cursivabar.addActionListener(new StyledEditorKit.BoldAction());
+		 * negritabar.addActionListener(new StyledEditorKit.ItalicAction());
+		 * subrabar.addActionListener(new StyledEditorKit.UnderlineAction());
+		 * verde.addActionListener(new StyledEditorKit.ForegroundAction("verde",
+		 * Color.GREEN)); rojo.addActionListener(new
+		 * StyledEditorKit.ForegroundAction("rojo",Color.RED));
+		 * azul.addActionListener(new
+		 * StyledEditorKit.ForegroundAction("azul",Color.BLUE));
+		 * centrado.addActionListener(new StyledEditorKit.AlignmentAction("centrado",
+		 * 1)); izquierda.addActionListener(new
+		 * StyledEditorKit.AlignmentAction("izquierda",0));
+		 * derecha.addActionListener(new StyledEditorKit.AlignmentAction("derecha",2));
+		 * justificado.addActionListener(new
+		 * StyledEditorKit.AlignmentAction("justificar",3));
+		 * 
+		 * 
+		 * menubar.add(negritabar); menubar.add(cursivabar); menubar.add(subrabar);
+		 * menubar.add(verde); menubar.add(rojo); menubar.add(azul);
+		 * menubar.add(centrado); menubar.add(izquierda); menubar.add(derecha);
+		 * menubar.add(justificado);
+		 */
+
+		menubar=new JToolBar();
 		
-		JToolBar menubar=new JToolBar();
-		JButton cursivabar=new JButton(new ImageIcon("C:/Users/genesis/Desktop/Cursos/JAVA/PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif"/*.../PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif"*/));
-		JButton subrabar=new JButton(new ImageIcon("src/subrayada.png"));
-		JButton negritabar=new JButton(new ImageIcon("C:/Users/genesis/Desktop/Cursos/JAVA/PROCESADOR DE TEXTOS/src/iconos/texto-en-negrita.gif"/*.../PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif"*/));
-		JButton verde=new JButton(new ImageIcon("src/verde.png"));
-		JButton rojo=new JButton(new ImageIcon("src/rojo.png"));
-		JButton azul=new JButton(new ImageIcon("src/azul.png"));
-		JButton centrado=new JButton(new ImageIcon("src/centrado.png"));
-		JButton izquierda=new JButton(new ImageIcon("src/izquierda.png"));
-		JButton derecha=new JButton(new ImageIcon("src/derecha.png"));
-		JButton justificado=new JButton(new ImageIcon("src/justificar.png"));
 		
-		cursivabar.addActionListener(new StyledEditorKit.BoldAction());
-		negritabar.addActionListener(new StyledEditorKit.ItalicAction());
-		subrabar.addActionListener(new StyledEditorKit.UnderlineAction());
-		verde.addActionListener(new StyledEditorKit.ForegroundAction("verde", Color.GREEN));
-		rojo.addActionListener(new StyledEditorKit.ForegroundAction("rojo",Color.RED));
-		azul.addActionListener(new StyledEditorKit.ForegroundAction("azul",Color.BLUE));
-		centrado.addActionListener(new StyledEditorKit.AlignmentAction("centrado", 1));
-		izquierda.addActionListener(new StyledEditorKit.AlignmentAction("izquierda",0));
-		derecha.addActionListener(new StyledEditorKit.AlignmentAction("derecha",2));
-		justificado.addActionListener(new StyledEditorKit.AlignmentAction("justificar",3));
+		//como devuelve un boton, podemos ponerlo a la escucha
+		configura_barra("../PROCESADOR DE TEXTOS/src/iconos/boton-de-cursiva.gif").addActionListener(new StyledEditorKit.ItalicAction());
+		configura_barra("../PROCESADOR DE TEXTOS/src/iconos/texto-en-negrita.gif").addActionListener(new StyledEditorKit.BoldAction());
+		configura_barra("src/subrayada.png").addActionListener(new StyledEditorKit.UnderlineAction());
+		menubar.addSeparator();
+		configura_barra("src/verde.png").addActionListener(new StyledEditorKit.ForegroundAction("verde",Color.GREEN));
+		configura_barra("src/rojo.png").addActionListener(new StyledEditorKit.ForegroundAction("rojo",Color.RED));
+		configura_barra("src/azul.png").addActionListener(new StyledEditorKit.ForegroundAction("azul", Color.BLUE));
+		menubar.addSeparator();
+		configura_barra("src/centrado.png").addActionListener(new StyledEditorKit.AlignmentAction("centrado", 1));
+		configura_barra("src/izquierda.png").addActionListener(new StyledEditorKit.AlignmentAction("izquierda", 0));
+		configura_barra("src/derecha.png").addActionListener(new StyledEditorKit.AlignmentAction("derecha", 2));
+		configura_barra("src/justificar.png").addActionListener(new StyledEditorKit.AlignmentAction("justificada", 3));
 		
-		
-		menubar.add(negritabar);
-		menubar.add(cursivabar);
-		menubar.add(subrabar);
-		menubar.add(verde);
-		menubar.add(rojo);
-		menubar.add(azul);
-		menubar.add(centrado);
-		menubar.add(izquierda);
-		menubar.add(derecha);
-		menubar.add(justificado);
 		
 		menubar.setOrientation(1);
-		
-		add(menubar,BorderLayout.WEST);
 
+		add(menubar, BorderLayout.WEST);
+
+	}
+	
+	public JButton configura_barra(String ruta) {
+		
+		JButton boton=new JButton(new ImageIcon(ruta));
+		menubar.add(boton);
+		return boton;
 	}
 
 	public void creacion_items(String menu_pertenece, String nom_item, String tipo_fuente, int tipo_estilo,
@@ -169,10 +197,10 @@ class panelMenu extends JPanel {
 		} else if (menu_pertenece == "estilo") {
 			estilo.add(itemMenu);
 			if (tipo_estilo == Font.BOLD) {
-				itemMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,InputEvent.CTRL_DOWN_MASK));
+				itemMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
 				itemMenu.addActionListener(new StyledEditorKit.BoldAction());
 			} else if (tipo_estilo == Font.ITALIC) {
-				itemMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I,InputEvent.CTRL_DOWN_MASK));
+				itemMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, InputEvent.CTRL_DOWN_MASK));
 				itemMenu.addActionListener(new StyledEditorKit.ItalicAction());
 			}
 		} else if (menu_pertenece == "tamanio") {
@@ -185,4 +213,6 @@ class panelMenu extends JPanel {
 	JMenuBar menu;
 	Font letra;
 	JTextPane laminaTexto;
+	JButton negitabarra, cusivabarra,subrayadabarra,amarillo,rojo,azul,izquierda,derecha,centrado,justificado;
+	JToolBar menubar;
 }
