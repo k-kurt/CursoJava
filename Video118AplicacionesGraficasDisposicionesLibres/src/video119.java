9112093bc0fe6dedc6e3cc17f37d894ef1984d28
kevin sboboda <sboboda1997@gmail.com>
2022-06-07 21:08:21 -0300

    Video 119_Creando plantilla propia para ubicar elementos

---
+++ Video118AplicacionesGraficasDisposicionesLibres/src/video119.java
@@ -0,0 +1,100 @@
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Label;
+import java.awt.LayoutManager;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class video119 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		marco marco1 = new marco();
+		marco1.setVisible(true);
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+class marco extends JFrame {
+	public marco() {
+		setTitle("Video 119 disposiciones libres II");
+		setBounds(200, 200, 800, 500);
+		panel lamina = new panel();
+		add(lamina);
+	}
+}
+
+class panel extends JPanel {
+	public panel() {
+
+		setLayout(new columnaContenerdor());
+		Label labnombre = new Label("NOMBRE: ");
+		Label labapellido = new Label("APELLIDO: ");
+		JTextField txtnombre = new JTextField();
+		JTextField txtapellido = new JTextField();
+
+		add(labnombre);
+		add(txtnombre);
+		add(labapellido);
+		add(txtapellido);
+
+	}
+}
+
+class columnaContenerdor implements LayoutManager {
+
+	@Override
+	public void addLayoutComponent(String name, Component comp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeLayoutComponent(Component comp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Dimension preferredLayoutSize(Container parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dimension minimumLayoutSize(Container parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void layoutContainer(Container parent) {
+		// TODO Auto-generated method stub
+		int contador = 0;
+
+		int n = parent.getComponentCount();// para utilizar en el for
+		for (int i = 0; i < n; i++) {
+			contador++;
+			Component c = parent.getComponent(i);// devuelve un objeto de tipo componente y almacena ese componente que
+													// devuelve este metodo parent
+
+			c.setBounds(x, y, 100, 20);
+			x += 100;
+			if (contador % 2 == 0) {
+				x = 20;
+				y += 40;
+
+			}
+		}
+
+	}
+
+	int x = 20;
+	int y = 20;
+}
