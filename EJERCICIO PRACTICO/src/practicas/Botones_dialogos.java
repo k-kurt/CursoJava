b9bd6d1d0c5f98f8231457bc1bccd2e19128faae
kevin sboboda <sboboda1997@gmail.com>
2022-07-15 20:41:36 -0300

    Video 124

--- EJERCICIO PRACTICO/src/practicas/Botones_dialogos.java
+++ EJERCICIO PRACTICO/src/practicas/Botones_dialogos.java
@@ -1,5 +1,25 @@
 package practicas;
 
-public class Botones_dialogos {
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class Botones_dialogos	extends JPanel {
+
+	public Botones_dialogos(String titulo, String[] opciones) {
+
+		setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),titulo));
+		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		ButtonGroup grupo=new ButtonGroup();
+		for(int i=0;i<opciones.length;i++) {
+			JRadioButton boton=new JRadioButton(opciones[i]);
+			add(boton);
+			grupo.add(boton);
+			boton.setSelected(i==0);
+		}
+		
+	}
 
 }
--- EJERCICIO PRACTICO/src/practicas/Marco_Dialogos.java
+++ EJERCICIO PRACTICO/src/practicas/Marco_Dialogos.java
@@ -5,15 +5,33 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Marco_Dialogos extends JFrame{
-	
+public class Marco_Dialogos extends JFrame {
+
 	public Marco_Dialogos() {
 		setTitle("Prueba de Diálogos");
 		setBounds(200, 100, 800, 500);
+
+		JPanel lamina = new JPanel();
+		lamina.setLayout(new GridLayout(2, 3));
+
+		String opcion1[] = { "Mensaje", "Confirmar", "Opcion", "Entrada" };
 		
 		
-		JPanel lamina=new JPanel();
-		lamina.setLayout(new GridLayout(2,3));
+		lamina_tipo=new Botones_dialogos("Tipo",opcion1);
 		
+		lamina.add(lamina_tipo);
+		add(lamina);
+		
+		
+		
+		
+		// Botones_dialogos lamina2=new Botones_dialogos("Tipo",new String[]{"Mensaje","Confirmar","Opcion","Entrada"});
+		// se puede instanciar directamente el array
+
+		//lamina.add(lamina2);
+		//add(lamina);
 	}
+	
+	private Botones_dialogos lamina_tipo;
+
 }
