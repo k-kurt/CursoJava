3af5220f1ca193d674f1e3a3a88b6664de37541a
kevin sboboda <sboboda1997@gmail.com>
2022-07-21 16:52:21 -0300

    Video 125

--- EJERCICIO PRACTICO/src/practicas/Marco_Dialogos.java
+++ EJERCICIO PRACTICO/src/practicas/Marco_Dialogos.java
@@ -1,7 +1,9 @@
 package practicas;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -18,9 +20,27 @@ public class Marco_Dialogos extends JFrame {
 		
 		
 		lamina_tipo=new Botones_dialogos("Tipo",opcion1);
+		lamina_tipo_mensaje=new Botones_dialogos("Tipo de Mensaje",new String[] {"ERROR_MENSSAGE","INFORMATION_MESSAGE","QUESTION_MESSAGE","PLAIN_MESSAGE"});
+		lamina_mensaje=new Botones_dialogos("Mensaje",new String[] {"Cadena","Icono","Componente","Otros","Object[]"});
+		lamina_confirmar=new Botones_dialogos("Confirmar", new String[] {"DEFAULT_OPTION","YES_NO_OPTION","YES_NO_CANCEL_OPTION","OK_CANCEL_OPTION"});
+		lamina_opcion=new Botones_dialogos("Opcion", new String[] {"String[]","Icon[]","Object[]"});
+		lamina_entrada=new Botones_dialogos("Entrada", new String[] {"Campo de texto","Combo"});
+		
+		setLayout(new BorderLayout());
 		
 		lamina.add(lamina_tipo);
-		add(lamina);
+		lamina.add(lamina_tipo_mensaje);
+		lamina.add(lamina_mensaje);
+		lamina.add(lamina_confirmar);
+		lamina.add(lamina_opcion);
+		lamina.add(lamina_entrada);
+		
+		//Contruimos la lamina inferior
+		JPanel lamina_inferior=new JPanel();
+		JButton btnMostrar=new JButton("Mostrar");
+		lamina_inferior.add(btnMostrar);
+		add(lamina,BorderLayout.CENTER);
+		add(lamina_inferior,BorderLayout.SOUTH);
 		
 		
 		
@@ -32,6 +52,6 @@ public class Marco_Dialogos extends JFrame {
 		//add(lamina);
 	}
 	
-	private Botones_dialogos lamina_tipo;
+	private Botones_dialogos lamina_tipo, lamina_tipo_mensaje, lamina_mensaje, lamina_confirmar, lamina_opcion, lamina_entrada;
 
 }
