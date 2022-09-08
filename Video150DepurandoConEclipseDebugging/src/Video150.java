028e7f9da1fa6911a5808c9bb6913caef5b5c7a8
kevin sboboda <sboboda1997@gmail.com>
2022-09-08 12:10:05 -0300

    Video 151

--- Video150DepurandoConEclipseDebugging/src/Video150.java
+++ Video150DepurandoConEclipseDebugging/src/Video150.java
@@ -21,3 +21,21 @@ public class Video150 {
 	}
 
 }
+
+
+
+/*
+ * En el video 151 vemos en la vista debug en el apartado de breakpoints que se
+ * puede poner un condicional para hacer puntos de interrupcion
+ * veos en las variables que se detuvo el array en el 5 por que eso le indicamos 
+ * en el condicional que i==5
+ * 
+ * tambien tenemos el apartado expressions que esta junto a variables y brackpoints e la vista debug
+ * sino esta entonces lo buscamos en las opciones de windows 
+ * ponemos pj elem_aleat[7] y cuando el array llegue a ese numero veremos que valor tiene
+ * tenemos que desactivar en este caso el condicional anterior
+ * 
+ *  
+ *  el boton step into se utiliza para entrar en el archivo donde se esta ejecutando
+ *  o retornando el valor que se esta ejecutando
+ */
