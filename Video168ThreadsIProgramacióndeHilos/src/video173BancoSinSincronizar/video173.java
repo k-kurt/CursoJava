a3a2ac19e7895d6fad1312c9f5cc4dd80d7841c8
kevin sboboda <sboboda1997@gmail.com>
2022-10-21 20:56:34 -0300

    video 173

--- Video168ThreadsIProgramacióndeHilos/src/video173BancoSinSincronizar/video173.java
+++ Video168ThreadsIProgramacióndeHilos/src/video173BancoSinSincronizar/video173.java
@@ -8,3 +8,35 @@ public class video173 {
 	}
 
 }
+
+class banco{
+	public banco() {
+		cuentas = new double[100];
+		
+		for(int i=0;i<cuentas.length;i++) {
+			cuentas[i]=2000;//cargamos cada elemento i del array cuentas con 2000
+		}
+		
+	}
+	
+	public void tranferencia(int cuentaOrigen, int cuentaDestino, double cantidad) {
+		if(cuentas[cuentaOrigen]<cantidad) {//evalua que el saldo no sea inferior a la cantidad
+			return;
+		}
+		System.out.println(Thread.currentThread());//sentencia que nos indica que thread se esta ejecutando
+		cuentas[cuentaOrigen]-=cantidad;//restamos la cantidad de la tranferencia
+		System.out.printf("%10.2f de %d para %d",cantidad, cuentaOrigen,cuentaDestino);//la sentencia printf
+		cuentas[cuentaDestino]+=cantidad;
+		System.out.printf(" Saldo Total: %10.2f/n",getSaldoTotal());
+	}
+	
+	public double getSaldoTotal() {
+		double suma_cuentas=0;
+		for(double a:cuentas) {//recorremos el array cuentas
+			suma_cuentas+=a;
+		}
+		return suma_cuentas;
+	}
+	
+	private final double[] cuentas;
+}
