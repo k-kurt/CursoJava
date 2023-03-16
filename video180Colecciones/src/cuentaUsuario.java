0caebd213c071d8f0323d1f62a0eb750afa1a0d4
k-kurt <sboboda1997@gmail.com>
2023-03-16 18:33:59 -0300

    parte del video 181

--- video180Colecciones/src/cuentaUsuario.java
+++ video180Colecciones/src/cuentaUsuario.java
@@ -8,17 +8,18 @@ import java.util.*;
         cliente cl2=new cliente("jorge", "00002", 300000);
         cliente cl3=new cliente("milagros", "00003", 350000);
         cliente cl4=new cliente("pajarito", "00004", 180000);
+        cliente cl5=new cliente("pajarito", "00004", 180000);
 
 
         Set <cliente> cuentaUsuario=new HashSet<cliente>();
             /* esto es programacion generica */
 
-
+ 
      cuentaUsuario.add(cl1);
      cuentaUsuario.add(cl2);
      cuentaUsuario.add(cl3);
      cuentaUsuario.add(cl4);
-
+     cuentaUsuario.add(cl5);
         for (cliente cliente : cuentaUsuario) {
             System.out.println(cliente.getNombre()+" "+cliente.getN_cuenta()+" "+cliente.getSaldo());
         }
