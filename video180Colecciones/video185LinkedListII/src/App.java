2fe280690e9e638e1f4074225fa7ed1455951830
k-kurt <sboboda1997@gmail.com>
2023-03-30 17:57:10 -0300

    video 185

--- video180Colecciones/video185LinkedListII/src/App.java
+++ video180Colecciones/video185LinkedListII/src/App.java
@@ -14,16 +14,51 @@ public class App {
          LinkedList<String> capitales=new LinkedList<String>();
 
         capitales.add("madrid");
-        capitales.add("medellin");
+        capitales.add("bogota");
         capitales.add("ciudad de mexico");
         capitales.add("lima");
 
         // System.out.println(paises+"    "+capitales);
 
+// 
+// agregar las capitales a la coleccion de paises
+//         
         ListIterator<String> iterA=paises.listIterator();
         ListIterator<String> iterB=capitales.listIterator();
 
         
+        while(iterB.hasNext()){
+            if (iterA.hasNext()) {
+                iterA.next();
+            }
+            iterA.add(iterB.next());
+        }
+        System.out.println(paises);
+// 
+//  eliminar las capitales e las posiciones par
+//         
+        iterB=capitales.listIterator(); //volver el iterador al comienzo 
+
+        while(iterB.hasNext()){
+            iterB.next();
+            if(iterB.hasNext()){
+                iterB.next();
+                iterB.remove();
+            }
+        }
+        System.out.println(capitales);
+
+// 
+// eliminar las que coinciden en la coleccion capitales
+// 
+
+        paises.removeAll(capitales);
+        System.out.println(paises);
 
     }
+
+    
+
+    
+
 }
