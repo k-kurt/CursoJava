a030ffbabee492cf82735f38b3b74913e7486709
k-kurt <sboboda1997@gmail.com>
2025-04-21 21:56:44 -0300

    videeo 194 mmin 8:51

--- video190Sockets/SocketsIIICrandoChatI/src/Servidor.java
+++ video190Sockets/SocketsIIICrandoChatI/src/Servidor.java
@@ -1,101 +1,105 @@
 
-
 import javax.swing.*;
 
 import java.awt.*;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Servidor  {
+public class Servidor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		MarcoServidor mimarco=new MarcoServidor();
-		
-		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-	}	
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        MarcoServidor mimarco = new MarcoServidor();
+
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
 }
 
-class MarcoServidor extends JFrame implements Runnable{
-	
-	public MarcoServidor(){
-		
-		setBounds(1200,300,280,350);				
-			
-		JPanel milamina= new JPanel();
-		
-		milamina.setLayout(new BorderLayout());
-		
-		areatexto=new JTextArea();
-		
-		milamina.add(areatexto,BorderLayout.CENTER);
-		
-		add(milamina);
-		
-		setVisible(true);
+class MarcoServidor extends JFrame implements Runnable {
 
+    public MarcoServidor() {
 
-		//para que este a la escucha el marco apenas iniciamos el programa
-		//es un hilo que permite hacer todo esto en segundo plano
-		Thread miThread=new Thread(this);
-		miThread.start();
-		
-		}
-	
-	private	JTextArea areatexto;
+        setBounds(1200, 300, 280, 350);
 
-	@Override
-	public void run() {
-	
-	
+        JPanel milamina = new JPanel();
 
-		try {
+        milamina.setLayout(new BorderLayout());
 
-			 String nick, ip, mensaje;
+        areatexto = new JTextArea();
 
-			EnvioPaquetes paquete_recibido;
+        milamina.add(areatexto, BorderLayout.CENTER);
 
+        add(milamina);
 
-				//ponemos a la escucha a la aplicacion con el puerto 9999  
-			ServerSocket server=new ServerSocket(9999);
-				//en un bucle infinito para poder abrir el puerto y aceptar la tranferencia de paquetes			
-			while(true){
-			Socket miServer=server.accept();
-				//creamos el flujo de datos
-			ObjectInputStream flujoDatosEntrada=new ObjectInputStream(miServer.getInputStream());
-				//guardamos el objecto en la variable instanciada de la clase Enviopaquetes
-			paquete_recibido=(EnvioPaquetes) flujoDatosEntrada.readObject();
-				//agregamos los datos a las variables
-			nick=paquete_recibido.getNick();
-			ip=paquete_recibido.getIp();
-			mensaje=paquete_recibido.getMensaje();
-				//mostramos en el textarea los datos
-			areatexto.append("\n"+nick+": "+ mensaje+" para: "+ ip );
+        setVisible(true);
 
-			/*
+        //para que este a la escucha el marco apenas iniciamos el programa
+        //es un hilo que permite hacer todo esto en segundo plano
+        Thread miThread = new Thread(this);
+        miThread.start();
+
+    }
+
+    private JTextArea areatexto;
+
+    @Override
+    public void run() {
+
+        try {
+
+            String nick, ip, mensaje;
+
+            EnvioPaquetes paquete_recibido;//informacion que le llega en ese paquete por la red
+
+            //ponemos a la escucha a la aplicacion con el puerto 9999  
+            ServerSocket server = new ServerSocket(9999);
+            //en un bucle infinito para poder abrir el puerto y aceptar la tranferencia de paquetes			
+            while (true) {
+                Socket miServer = server.accept();
+                //creamos el flujo de datos
+                ObjectInputStream flujoDatosEntrada = new ObjectInputStream(miServer.getInputStream());//el input para recibir, el output para enviar
+                //guardamos el objecto en la variable instanciada de la clase Enviopaquetes
+                paquete_recibido = (EnvioPaquetes) flujoDatosEntrada.readObject();
+                //agregamos los datos a las variables
+                nick = paquete_recibido.getNick();
+                ip = paquete_recibido.getIp();
+                mensaje = paquete_recibido.getMensaje();
+                //mostramos en el textarea los datos
+                areatexto.append("\n" + nick + ": " + mensaje + " para: " + ip);
+
+                //necesitamos crear un socket el cual es el medio por cual va a viajar los datos desde el servidor al otro cliente
+                Socket enviaDestinatario = new Socket(ip, 9090);
+                
+                ObjectOutputStream paqueteReenvio=new ObjectOutputStream(enviaDestinatario.getOutputStream());//creamos un paquete en el cual usamos el socket para enviarlo
+                
+                paqueteReenvio.writeObject(paquete_recibido);//en ese objeto metemos con los datos del paquete recibido
+                
+                enviaDestinatario.close();//cerramos el socket
+                
+
+                miServer.close();
+
+                /*
 			//creamos el flujo de datos, el socket ya esta creado
 			DataInputStream miInput= new DataInputStream(miServer.getInputStream());
 			//guardamos en un string el texto  escrito en el cliente
 			String text_mensaje=miInput.readUTF();
 			//lo pegamos en el campo de texto del servidor
 			areatexto.append("\n"+text_mensaje);
-			miServer.close();
-			 */
-			}
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		
-		}
-		
+			
+                 */
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
 
+        }
 
-
-
-	}
+    }
 
 }
